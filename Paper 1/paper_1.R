a42403b23d1961929c094b0b6ba8c13956b7812d
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-09-22 17:05:42 -0700

    Comments in code

--- Paper 1/paper_1.R
+++ Paper 1/paper_1.R
@@ -7,13 +7,14 @@ compensation <- read.csv("Employee_Compensation_20250829.csv")
 compensation <- na.omit(compensation)
 #drop rows with missing values
 compensation <- compensation[complete.cases(compensation[, c("Total.Benefits", "Total.Compensation")]), ]
-#deal with commas
+#deal with commas (e.g. 1,000 = 1000)
 compensation$Total.Benefits <- as.numeric(gsub(",", "", compensation$Total.Benefits))
 compensation$Total.Salary <- as.numeric(gsub(",", "", compensation$Total.Salary))
 
 ## build regression model for total compensation as the predictor variable for total benefits
 model <- lm(Total.Benefits ~ Total.Salary, data=compensation)
 
+#print out a summary of the regression model (e.g. estimated slope, p-value, etc.)
 summary(model)
 
 #plot the data
